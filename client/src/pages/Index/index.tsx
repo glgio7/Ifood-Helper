711ef2b071ff107cea5ec551b1c59db49bf47d48
GLGIO7 <giovanelucas00@outlook.com>
2023-06-06 04:38:47 -0300

    feat: add marker on clicking map

--- client/src/pages/Index/index.tsx
+++ client/src/pages/Index/index.tsx
@@ -1,44 +1,118 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+	MapContainer,
+	Marker,
+	Popup,
+	TileLayer,
+	useMapEvents,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import * as S from "./styles";
 import { useEffect, useState } from "react";
 import Loading from "../../components/Loading";
 import { Icon } from "leaflet";
-import currentIconLoc from "../../assets/current-loc.svg";
+import { HandleClickMapProps, IMarker } from "./types";
 
-const Home = () => {
-	const [lat, setLat] = useState<number | undefined>();
-	const [long, setLong] = useState<number | undefined>();
+//Icons
 
-	const customIcon = new Icon({
-		iconUrl: currentIconLoc,
+const icons: Icon[] = [
+	new Icon({ iconUrl: "/icons/current-icon.svg", iconSize: [36, 36] }),
+];
+
+// Events
+
+const HandleClickMap = ({ markers, setMarkers }: HandleClickMapProps) => {
+	const currentIcon = new Icon({
+		iconUrl: icons[0].options.iconUrl,
 		iconSize: [36, 36],
 	});
 
+	useMapEvents({
+		click(e) {
+			const addMarker = window.confirm("Adicionar marcador?");
+
+			if (addMarker) {
+				const lat = e.latlng.lat;
+				const lng = e.latlng.lng;
+
+				setMarkers((prevMarkers) => {
+					return [
+						...prevMarkers,
+						{
+							icon: currentIcon,
+							comment: `Localização. ${markers.length + 1}`,
+							position: [lat, lng],
+						},
+					];
+				});
+			}
+		},
+	});
+
+	return null;
+};
+
+const Home = () => {
+	const [markers, setMarkers] = useState<IMarker[]>([]);
+	const [currentLocation, setCurrentLocation] = useState<IMarker>(
+		{} as IMarker
+	);
+
 	useEffect(() => {
+		const currentIcon = icons.find(
+			(icon) => icon.options.iconUrl === "/icons/current-icon.svg"
+		);
+
 		navigator.geolocation.getCurrentPosition(function (position) {
-			setLat(position.coords.latitude);
-			setLong(position.coords.longitude);
+			const lat = position.coords.latitude;
+			const lng = position.coords.longitude;
+			setCurrentLocation({
+				icon: currentIcon!,
+				comment: "Você está aqui.",
+				position: [lat, lng],
+			});
+
+			if (
+				markers.filter((marker) => marker.comment === "Você está aqui.")
+					.length < 1
+			) {
+				setMarkers([
+					{
+						icon: currentIcon!,
+						comment: "Você está aqui.",
+						position: [lat, lng],
+					},
+				]);
+			}
 		});
 	}, []);
 
 	return (
 		<S.Home>
-			{(!lat || !long) && <Loading />}
-			{lat && long !== undefined && (
-				<MapContainer
-					center={[lat, long]}
-					zoom={13}
-					className="leaftlet-container"
-				>
-					<TileLayer
-						attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-					/>
-					<Marker position={[lat, long]} icon={customIcon}>
-						<Popup>You're here.</Popup>
-					</Marker>
-				</MapContainer>
+			{!currentLocation.position && <Loading />}
+			{currentLocation.position !== undefined && (
+				<S.Container>
+					<MapContainer
+						center={currentLocation.position}
+						zoom={13}
+						className="leaftlet-container"
+					>
+						<HandleClickMap markers={markers} setMarkers={setMarkers} />
+						<TileLayer
+							attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+							url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+						/>
+						{markers.map((marker) => (
+							<Marker
+								position={marker.position}
+								icon={marker.icon}
+								key={marker.position.toString()}
+							>
+								<Popup className="custom-popup">{marker.comment}</Popup>
+							</Marker>
+						))}
+					</MapContainer>
+					<S.CustomPopup></S.CustomPopup>
+				</S.Container>
 			)}
 		</S.Home>
 	);
