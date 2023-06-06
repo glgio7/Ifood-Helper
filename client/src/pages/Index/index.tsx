80a59ee63848747c9a35a43ef5d50d48dba6ef75
GLGIO7 <giovanelucas00@outlook.com>
2023-06-06 00:02:28 -0300

    feat: add map from leaflet

--- client/src/pages/Index/index.tsx
+++ client/src/pages/Index/index.tsx
@@ -1,7 +1,30 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import * as S from "./styles";
+import { useState } from "react";
 
 const Home = () => {
-	return <S.Home></S.Home>;
+	const [lat, setLat] = useState<number | undefined>(-23.5489);
+	const [long, setLong] = useState<number | undefined>(-46.6388);
+	return (
+		<S.Home>
+			{lat && long !== undefined && (
+				<MapContainer
+					center={[lat, long]}
+					zoom={13}
+					className="leaftlet-container"
+				>
+					<TileLayer
+						attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					/>
+					<Marker position={[lat, long]}>
+						<Popup>You're here.</Popup>
+					</Marker>
+				</MapContainer>
+			)}
+		</S.Home>
+	);
 };
 
 export default Home;
--- client/src/pages/Index/styles.ts
+++ client/src/pages/Index/styles.ts
@@ -4,7 +4,13 @@ export const Home = styled.main`
 	min-height: calc(100vh - 64px);
 	width: 100%;
 
-	background-image: url("/images/example-map.png");
+	/* background-image: url("/images/example-map.png"); */
 	background-size: cover;
 	background-repeat: no-repeat;
+
+	.leaftlet-container {
+		height: calc(100vh - 64px);
+		width: 100%;
+		z-index: 90;
+	}
 `;
