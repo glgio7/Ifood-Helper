b5447c12e1ca5649cf131f1d3dc3e1e4cd697cae
GLGIO7 <giovanelucas00@outlook.com>
2023-06-11 12:38:27 -0300

    feat: add create-marker repository

--- server/src/repositories/markers/mongo-create-marker.ts
+++ server/src/repositories/markers/mongo-create-marker.ts
@@ -1,1 +1,31 @@
-export {};
+import { MongoClientMarkers } from "../../database/mongo";
+import { IMarker } from "../../entities/markers/protocols";
+import { ICreateMarkerRepository } from "../../use-cases/create-marker/protocols";
+
+export class CreateMarkerRepositoy implements ICreateMarkerRepository {
+	async createMarker(params: IMarker): Promise<IMarker> {
+		const { position, author, comment, icon } = params;
+
+		const markerAlreadyExists = await MongoClientMarkers.db
+			.collection("markers")
+			.findOne({ position: position });
+
+		if (markerAlreadyExists) {
+			throw new Error("Marker is already in db.");
+		}
+
+		const { insertedId } = await MongoClientMarkers.db
+			.collection("markers")
+			.insertOne({ position, author, comment, icon });
+
+		const marker = await MongoClientMarkers.db
+			.collection<Omit<IMarker, "id">>("markers")
+			.findOne({ _id: insertedId });
+
+		if (!marker) {
+			throw new Error("Marker not created!");
+		}
+
+		return marker;
+	}
+}
