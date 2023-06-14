2bc10e715b245061b03f600f1c0ec9bac2243053
GLGIO7 <giovanelucas00@outlook.com>
2023-06-14 16:26:39 -0300

    feat: about page done

--- client/src/pages/About/index.tsx
+++ client/src/pages/About/index.tsx
@@ -3,7 +3,63 @@ import * as S from "./styles";
 const About = () => {
 	return (
 		<S.About>
-			<h2>Ifood Helper - Sobre</h2>
+			<img src="/images/about.svg" alt="Sobre" className="title-image" />
+			<h2>Sobre o aplicativo</h2>
+			<p>
+				O Ifood Helper foi projetado para ajudar você a{" "}
+				<strong>aproveitar ao máximo</strong> sua experiência com o iFood.
+				Queremos tornar sua jornada mais segura e conveniente, fornecendo
+				informações úteis sobre sua localização e alertando sobre possíveis
+				problemas nas áreas ao seu redor. Aqui está o que você precisa saber
+				sobre o nosso aplicativo:
+			</p>
+			<p>
+				A <strong>interface</strong> do Ifood Helper foi inspirada no aplicativo
+				iFood para Entregadores, um dos aplicativos de delivery mais utilizados.
+				Isso torna a navegação familiar e intuitiva, facilitando o uso do nosso
+				aplicativo.
+			</p>
+			<h3>Funcionalidades</h3>
+			<p>
+				<strong>Integração com API de Geolocalização:</strong> Nossa integração
+				com a API de Geolocalização permite que você compartilhe sua localização
+				com o aplicativo. Isso nos ajuda a fornecer alertas e informações
+				relevantes para a sua área.
+			</p>
+			<p>
+				<strong>Adicionar marcadores:</strong> Mesmo que você já não esteja mais
+				em uma determinada localização, você ainda pode adicionar marcadores
+				para alertar outros usuários sobre possíveis problemas. Queremos criar
+				uma comunidade colaborativa onde todos possam contribuir para a
+				segurança e o bem-estar uns dos outros.
+			</p>
+			<p>
+				<strong>Pontuação de interação:</strong> Reconhecemos e valorizamos sua
+				interação com o aplicativo. Por meio de sua participação ativa, você
+				ganhará pontos, incentivando uma comunidade engajada e responsável.
+			</p>
+			<p>
+				<strong>Agrupamento de marcadores:</strong> Quando você diminuir o zoom
+				no mapa, os marcadores serão agrupados, indicando a quantidade de
+				alertas naquela área específica. Isso permite que você tenha uma visão
+				geral dos problemas relatados.
+			</p>
+			<p>
+				<strong>Tipos de alertas:</strong> Inicialmente, os tipos de alertas
+				disponíveis são: área de risco, baixa iluminação, buracos na pista e
+				estabelecimentos com alta demanda. Essas informações iniciais nos
+				ajudarão a criar uma base sólida para a comunidade.
+			</p>
+			<h3>Considerações do criador</h3>
+			<p>
+				Espero que você aproveite ao máximo o Ifood Helper e que ele se torne um
+				aliado indispensável em suas experiências com o iFood. Me comprometo a
+				fornecer um serviço confiável e útil para a comunidade. Se você tiver
+				alguma dúvida ou sugestão, não hesite em entrar em contato.
+			</p>
+			<p>
+				<strong> Meus agradecimentos por juntar-se ao Ifood Helper!</strong>
+			</p>
 		</S.About>
 	);
 };
--- client/src/pages/About/styles.ts
+++ client/src/pages/About/styles.ts
@@ -4,12 +4,37 @@ export const About = styled.main`
 	min-height: calc(100vh - 64px);
 	width: 100%;
 
-	padding: 1rem;
+	padding: 2rem 1rem;
 
 	position: relative;
 
-	h2 {
-		width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	background-color: #fff;
+
+	.title-image {
+		width: 240px;
+		aspect-ratio: 1;
+
+		box-shadow: 30px 15px 30px rgba(0, 0, 0, 0.5);
+
+		border-radius: 50% 15%;
+
+		object-fit: cover;
+	}
+
+	h2,
+	h3 {
+		margin-block: 1rem;
+		color: #bb2222;
+		font-family: "Montserrat Alternates", sans-serif;
+	}
+
+	p {
+		margin-block: 0.5rem;
 		text-align: center;
+		max-width: 90%;
 	}
 `;
