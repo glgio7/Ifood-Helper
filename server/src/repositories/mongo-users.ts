998fbcc5e6b1863f4c4777f9e1126e69290568fc
GLGIO7 <giovanelucas00@outlook.com>
2023-06-19 22:54:29 -0300

    feat: add jsonwebtoken for login persistence

--- server/src/repositories/mongo-users.ts
+++ server/src/repositories/mongo-users.ts
@@ -1,8 +1,15 @@
+import { ObjectId } from "mongodb";
 import { MongoClientUsers } from "../database/mongo";
 import { IUser } from "../entities/user/protocols";
-import { IAuthWithEmail, IUserRepository } from "./protocols";
+import {
+	IAuthWithEmail,
+	IAuthWithToken,
+} from "../use-cases/users/auth-user/protocols";
+import { IUsersRepository } from "./protocols";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export class UserRepository implements IUserRepository {
+export class UsersRepository implements IUsersRepository {
 	async authWithEmail(
 		params: IAuthWithEmail
 	): Promise<Omit<IUser, "password">> {
@@ -11,8 +18,36 @@ export class UserRepository implements IUserRepository {
 			.collection<IUser>("users")
 			.findOne({ email });
 
-		if (!user || user.password !== password) {
+		const passwordMatch =
+			user && (await bcrypt.compare(password, user?.password));
+
+		if (!user || !passwordMatch) {
 			throw new Error("Credenciais inválidas.");
+		}
+
+		const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, {
+			expiresIn: "5d",
+		});
+
+		user.token = token;
+
+		return user;
+	}
+	async authWithToken(
+		params: IAuthWithToken
+	): Promise<Omit<IUser, "password">> {
+		const { token } = params;
+
+		const decodedToken = jwt.verify(token, process.env.JWT_SECRET!) as {
+			userId: string;
+		};
+
+		const user = await MongoClientUsers.db
+			.collection<IUser>("users")
+			.findOne({ _id: new ObjectId(decodedToken.userId) });
+
+		if (!decodedToken || !user) {
+			throw new Error("Token is invalid or expired.");
 		}
 
 		return user;
@@ -33,9 +68,18 @@ export class UserRepository implements IUserRepository {
 			throw new Error("O email ou username já está em uso.");
 		}
 
+		const hashedPassword = await bcrypt.hash(password, 10);
+
 		const { insertedId } = await MongoClientUsers.db
 			.collection("users")
-			.insertOne({ email, username, createdAt, name, score, password });
+			.insertOne({
+				email,
+				password: hashedPassword,
+				username,
+				createdAt,
+				name,
+				score,
+			});
 
 		const user = await MongoClientUsers.db
 			.collection<Omit<IUser, "id">>("users")
--- server/src/repositories/protocols.ts
+++ server/src/repositories/protocols.ts
@@ -1,17 +1,17 @@
 import { IMarker } from "../entities/marker/protocols";
 import { IUser } from "../entities/user/protocols";
+import {
+	IAuthWithEmail,
+	IAuthWithToken,
+} from "../use-cases/users/auth-user/protocols";
 
 export interface IMarkersRepository {
 	getMarkers(): Promise<IMarker[]>;
 	createMarker(params: IMarker): Promise<IMarker>;
 }
 
-export interface IAuthWithEmail {
-	email: string;
-	password: string;
-}
-
-export interface IUserRepository {
+export interface IUsersRepository {
 	createUser(params: IUser): Promise<IUser>;
 	authWithEmail(params: IAuthWithEmail): Promise<Omit<IUser, "password">>;
+	authWithToken(params: IAuthWithToken): Promise<Omit<IUser, "password">>;
 }
