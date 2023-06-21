289f2166a668a5a02e738f892bfdf30380ac8182
GLGIO7 <giovanelucas00@outlook.com>
2023-06-21 11:46:04 -0300

    feat: add api swagger doc

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,12 +1,22 @@
 import { Router } from "express";
 import { MarkersRoute } from "./markers";
 import { AuthRoutes } from "./users";
+import * as swaggerDocument from "../../swagger.json";
+import swaggerUi from "swagger-ui-express";
 
 export const createRoutes = () => {
 	const router = Router();
 
 	router.use("/markers", MarkersRoute());
 	router.use("/auth", AuthRoutes());
+	router.use(
+		"/",
+		swaggerUi.serve,
+		swaggerUi.setup(swaggerDocument, {
+			customSiteTitle: "Ifood Helper | API Doc",
+			customfavIcon: "https://ifoodhelper.vercel.app/favicon.png",
+		})
+	);
 
 	return router;
 };
